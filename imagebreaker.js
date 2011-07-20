0bb315a165b9a755b0ad3c3db29215e129f0cafd
Stephen Woods <saw@yahoo-inc.com>
2011-07-19 20:52:53 -0700

    working

--- imagebreaker.js
+++ imagebreaker.js
@@ -1,1 +1,61 @@
-var im = require('imagemagick');
+var im = require('imagemagick'),
+  http = require('http'),
+   url = require('url');
+
+function handle(req, res){
+		var respData;
+		
+		var urlData = url.parse(req.url,true);
+		// http://farm7.static.flickr.com/6002/5949520546_7ff09849d0_o.jpg
+		var g = http.request({
+			host:'farm' + urlData.query.farm + '.static.flickr.com',
+			port: 80,
+			path:urlData.query.path
+			//http://farm7.static.flickr.com/6002/5949520546_3b10f92a44_b.jpg
+		}, function(resp){
+			resp.setEncoding('binary');
+			respData = '';
+			
+			resp.on('data', function(chunk){
+			
+				respData += chunk;
+				
+			});
+			
+			resp.on('end', function(){
+				console.log('done loading data, beginning convert');
+				var b = new Buffer(respData);
+				
+				var im = require('imagemagick');
+				im.resize({
+					srcData:respData,
+					width:urlData.query.width,
+					sharpening:0.5,
+					format:'jpg',
+					quality:0.9,
+					filter:'Lagrange',
+					customArgs:['-crop',urlData.query.crop, '+repage']
+				}, function(err, stdout, stderr){
+					if(err){
+                       res.writeHead(404, {'Content-Type': 'text/html'});
+					   console.log(req.headers);
+                       console.log(err);
+                       res.end('Sorry, you did something stupid I bet. No image here.');
+					}else{
+		                res.writeHead(200, {'Content-Type': 'image/jpeg'});
+					    res.end(stdout, 'binary');
+                    }
+				});
+				
+				
+			})
+			
+		}).on('error', function(e) {
+			console.log(e.message);
+		});
+		
+		g.end();
+	
+}
+
+exports.handle = handle;
--- server.js
+++ server.js
@@ -6,8 +6,6 @@ http.createServer(function (req, res) {
 	var respData = '';
 	if(req.url === '/'){
 		res.writeHead(200, {'Content-Type': 'text/plain'});
-		
-		res.write('ok');
 		res.end('Yes, this is good');
 	}else if(req.url.match(/favicon\.ico/)){
 		
@@ -21,58 +19,13 @@ http.createServer(function (req, res) {
 		res.end(buf, 'binary');
 		
 	}else if(req.url.match(/crop/)){
-		res.writeHead(200, {'Content-Type': 'image/jpeg'});
-		var urlData = url.parse(req.url,true);
-		console.log(urlData.query.crop);
-		// http://farm7.static.flickr.com/6002/5949520546_7ff09849d0_o.jpg
-		var g = http.request({
-			host:'farm' + urlData.query.farm + '.static.flickr.com',
-			port: 80,
-			path:urlData.query.path
-			//http://farm7.static.flickr.com/6002/5949520546_3b10f92a44_b.jpg
-		}, function(resp){
-			resp.setEncoding('binary');
-			
-			
-			resp.on('data', function(chunk){
-			
-				respData += chunk;
-				
-			});
-			
-			resp.on('end', function(){
-				console.log('done loading data, beginning convert');
-				var b = new Buffer(respData);
-				
-				var im = require('imagemagick');
-				im.resize({
-					srcData:respData,
-					width:urlData.query.width,
-					sharpening:0.5,
-					format:'jpg',
-					quality:0.9,
-					filter:'Lagrange',
-					customArgs:['-crop',urlData.query.crop, '+repage']
-				}, function(err, stdout, stderr){
-					if(err){
-						throw err;
-					}
-					res.end(stdout, 'binary');
-				});
-				
-				
-			})
-			
-		}).on('error', function(e) {
-			console.log(e.message);
-		});
 		
-		g.end();
+		var breaker = require('./imagebreaker.js');
+		
+		breaker.handle(req, res);
+
 		
 	}
 	
 	
-}).listen(1337, "127.0.0.1");
-
-
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(8000);
